5b56ae40f1ed6d6e6fa2166adf0fec704a74db8a
jenni9805 <58237071+jenni9805@users.noreply.github.com>
2020-05-07 23:49:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pickionarygame/UnlockWords.kt
@@ -0,0 +1,12 @@
+package com.example.pickionarygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UnlockWords : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unlock_words)
+    }
+}
